e46eddb470eae51cd4631f7478cc5f271148000f
EdwinWalela <edwinwalela@gmail.com>
2021-06-08 11:45:35 +0300

    unpack vote from client

--- main.go
+++ main.go
@@ -1,11 +1,7 @@
 package main
 
 import (
-	"encoding/base64"
-	"fmt"
-	"log"
-
-	"github.com/edwinwalela/jamii-core/jcrypto"
+	"github.com/edwinwalela/jamii-core/primitives"
 )
 
 func main() {
@@ -27,25 +23,26 @@ func main() {
 
 	// fmt.Println(valid)
 
-	kp := &jcrypto.KeyPair{}
+	// kp := &jcrypto.KeyPair{}
 
-	decodedPub, err := base64.StdEncoding.DecodeString("oMIwjMspTa8oBTsc/0qJ/GUvc6Fa/MX78F2lwkNdePs=")
+	// decodedPub, err := base64.StdEncoding.DecodeString("oMIwjMspTa8oBTsc/0qJ/GUvc6Fa/MX78F2lwkNdePs=")
 
-	decodedSig, sErr := base64.StdEncoding.DecodeString("rgBcyHuuyeB9CgwtiG/+PG2JqRZLf3lPpsD5fSgANnQlQBrX78db874Xys5f/JnjZccXVMaUvqpzO3F/utatDw==")
+	// decodedSig, sErr := base64.StdEncoding.DecodeString("rgBcyHuuyeB9CgwtiG/+PG2JqRZLf3lPpsD5fSgANnQlQBrX78db874Xys5f/JnjZccXVMaUvqpzO3F/utatDw==")
 
-	if err != nil {
-		log.Println(err)
-	}
-	if sErr != nil {
-		log.Println(err)
-	}
+	// if err != nil {
+	// 	log.Println(err)
+	// }
+	// if sErr != nil {
+	// 	log.Println(err)
+	// }
 
-	jcrypto.PubKeyFromBytes(decodedPub, kp)
+	// jcrypto.PubKeyFromBytes(decodedPub, kp)
 
-	valid := jcrypto.VerifySig(decodedSig, []byte("hello world"), kp)
+	// valid := jcrypto.VerifySig(decodedSig, []byte("hello world"), kp)
 
-	fmt.Println(valid)
-	// v := &primitives.Vote{}
-	// v.UnpackClientString("32983498|82913|candidate,candidate,candidate|signature|timestamp")
+	// fmt.Println(valid)
+	v := &primitives.Vote{}
+	clientData := "309ecc489c12d6eb4cc40f50c902f2b4d0ed77ee511a7c7a9bcd3ca86d4cd86f989dd35bc5ff499670da34255b45b0cfd830e81f605dcf7dc5542e93ae9cd76f|Z/Yg3ETYDRBlXcDy09p/NOyQRGycYRU8kNPsTRkwqRYpiz8ljL87u8fg/x/xzpELh65Af95kIeV2zoV++CbiDw==|rLPdtJDVywkcufc0kSQpmGnQ8sc6frPvVbMWYfwkFj0=|rLPdtJDVywkcufc0kSQpmGnQ8sc6frPvVbMWYfwkFj0=,rLPdtJDVywkcufc0kSQpmGnQ8sc6frPvVbMWYfwkFj0=,rLPdtJDVywkcufc0kSQpmGnQ8sc6frPvVbMWYfwkFj0=,rLPdtJDVywkcufc0kSQpmGnQ8sc6frPvVbMWYfwkFj0="
+	v.UnpackClientString(clientData)
 
 }
--- primitives/vote.go
+++ primitives/vote.go
@@ -1,12 +1,12 @@
 package primitives
 
 import (
-	"crypto/ecdsa"
-	"crypto/elliptic"
-	"fmt"
-	"log"
-	"math/big"
+	"encoding/base64"
+	"strconv"
 	"strings"
+
+	"github.com/edwinwalela/jamii-core/jcrypto"
+	"github.com/katzenpost/core/crypto/eddsa"
 )
 
 type Vote struct {
@@ -15,42 +15,72 @@ type Vote struct {
 	Mobile clients submits vote via TCP socket connection & node packs it into a Vote
 	**/
 
-	Address    ecdsa.PublicKey   // public key of tx initiator
-	Candidates []ecdsa.PublicKey // list of candidtes selected by initiator
+	Address    eddsa.PublicKey   // public key of tx initiator
+	Candidates []eddsa.PublicKey // list of candidtes selected by initiator
 	Signature  []byte            // signature by the initiator's public key
 	Hash       string            // hash of the tx
 	Timestamp  uint64            // Unix timestamp of tx in seconds
 }
 
 /**
-	string data from client ->	x,y|candidate,candidate,candidate|signature|timestamp
+	string data from client ->	pubkey64|sign64|hash|candidate,candidate,candidate|timestamp
 	This is broken down by node
 **/
 
-func (v *Vote) UnpackClientString(data string) {
+func (v *Vote) UnpackClientString(data string) error {
+
 	vote := strings.Split(data, "|")
-	ecdsaX := new(big.Int)
-	ecdsaY := new(big.Int)
+
+	var sig64, pub64 string
+	var uintErr error
+
+	candidates := make([]eddsa.PublicKey, 1)
 
 	for i, val := range vote {
 		switch i {
+		case 0: // Extract vote hash
+			v.Hash = val
+		case 1: // Extract sign64
+			sig64 = val
+		case 2: // Extract pub64
+			pub64 = val
+		case 3: // Extract candidates []pub64
+			for _, candidatePub64 := range strings.Split(val, ",") {
+				decodedPub, pubErr := base64.StdEncoding.DecodeString(candidatePub64)
 
-		case 0: // Extract curve X value
-			ecdsaX, ok := ecdsaX.SetString(val, 10)
-			if !ok {
-				log.Println("SetString error")
+				if pubErr != nil {
+					return pubErr
+				}
+				kp := &jcrypto.KeyPair{}
+				jcrypto.PubKeyFromBytes(decodedPub, kp)
+				candidates = append(candidates, kp.PubKey)
 			}
-			fmt.Println("X-val:", ecdsaX)
-
-		case 1: // Extract curve Y value
-			ecdsaY, ok := ecdsaY.SetString(val, 10)
-			if !ok {
-				log.Println("SetString error")
+		case 4:
+			v.Timestamp, uintErr = strconv.ParseUint(val, 10, 64)
+			if uintErr != nil {
+				return uintErr
 			}
-			fmt.Println("Y-val:", ecdsaY)
 		}
 	}
 
-	v.Address = ecdsa.PublicKey{X: ecdsaX, Y: ecdsaY, Curve: elliptic.P256()}
+	decodedPub, pubErr := base64.StdEncoding.DecodeString(pub64)
+	decodedSig, sigErr := base64.StdEncoding.DecodeString(sig64)
 
+	if pubErr != nil {
+		return pubErr
+	}
+
+	if sigErr != nil {
+		return sigErr
+	}
+
+	kp := &jcrypto.KeyPair{}
+
+	jcrypto.PubKeyFromBytes(decodedPub, kp)
+
+	v.Address = kp.PubKey
+	v.Signature = decodedSig
+	v.Candidates = candidates
+
+	return nil
 }
